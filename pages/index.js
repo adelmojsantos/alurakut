2a8f4a2fb04b522d336718ba0ceb50a153d08509
Adelmo Santos <adelmojsantos1985@gmail.com>
2021-07-12 22:53:14 -0300

    primeiro dia

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,70 @@
-import styled from 'styled-components'
+import Box from '../src/components/Box';
+import MainGrid from '../src/components/MainGrid';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.gitHubUser}.png`} alt="foto Perfil"
+        style={{ borderRadius: '8px' }}
+      />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const gitHubUser = 'adelmojsantos';
+  const pessoasFavoritas = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'felipefialho',
+    'Tarcilio-Ba',
+    'diegohfcelestino'
+  ]
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar gitHubUser={gitHubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem Vindo(a)
+
+              <OrkutNostalgicIconSet />
+            </h1>
+          </Box>
+        </div>
+        <div className="profileRelationsArea"
+          style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade ({pessoasFavoritas.length})
+            </h2>
+
+            <ul>
+              {pessoasFavoritas.map((githubUser) => {
+                return (
+                  <li>
+                    <a href={`/users/${githubUser}`} key={githubUser}>
+                      <img src={`https://github.com/${githubUser}.png`} />
+                      <span>{githubUser}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
